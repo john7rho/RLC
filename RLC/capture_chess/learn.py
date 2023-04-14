53014dd119cb7cae909585856299517dbd74f29a
John Rho <55637821+john7rho@users.noreply.github.com>
2023-04-14 11:42:54 -0400

    Update learn.py
    Changed to deep q-learning method, need to test

--- RLC/capture_chess/learn.py
+++ RLC/capture_chess/learn.py
@@ -5,7 +5,16 @@ from chess.pgn import Game
 import pandas as pd
 
 
-class Q_learning(object):
+from RLC.capture_chess.agent import Agent
+from RLC.capture_chess.environment import Board
+import numpy as np
+from chess.pgn import Game
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.optimizers import Adam
+
+class DeepQ_learning(object):
 
     def __init__(self, agent, env, memsize=1000):
         """
@@ -22,97 +31,41 @@ class Q_learning(object):
         self.reward_trace = []
         self.memory = []
         self.sampling_probs = []
+        self.deep_q_network = self.build_deep_q_network()
 
-    def learn(self, iters=100, c=10):
-        """
-        Run the Q-learning algorithm. Play greedy on the final iter
-        Args:
-            iters: int
-                amount of games to train
-            c: int
-                update the network every c games
+    def build_deep_q_network(self):
+        model = Sequential()
+        model.add(Dense(64, input_shape=(8, 8, 8), activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(64 * 64, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=0.001))
+        return model
 
-        Returns: pgn (str)
-            pgn string describing final game
-
-        """
-        for k in range(iters):
-            if k % c == 0:
-                print("iter", k)
-                self.agent.fix_model()
-            greedy = True if k == iters - 1 else False
-            self.env.reset()
-            self.play_game(k, greedy=greedy)
-
-        pgn = Game.from_board(self.env.board)
-        reward_smooth = pd.DataFrame(self.reward_trace)
-        reward_smooth.rolling(window=10, min_periods=0).mean().plot()
-
-        return pgn
+    # Other methods remain the same, but modify the play_game method
 
     def play_game(self, k, greedy=False, maxiter=25):
-        """
-        Play a game of capture chess
-        Args:
-            k: int
-                game count, determines epsilon (exploration rate)
-            greedy: Boolean
-                if greedy, no exploration is done
-            maxiter: int
-                Maximum amount of steps per game
+        # Rest of the method remains the same
 
-        Returns:
-
-        """
-        episode_end = False
-        turncount = 0
-
-        # Here we determine the exploration rate. k is divided by 250 to slow down the exploration rate decay.
-        eps = max(0.05, 1 / (1 + (k / 250))) if not greedy else 0.
-
-        # Play a game of chess
-        while not episode_end:
-            state = self.env.layer_board
-            explore = np.random.uniform(0, 1) < eps  # determine whether to explore
-            if explore:
+        # Modify the action-value estimation using the deep Q-network
+        else:
+            action_values = self.deep_q_network.predict(np.expand_dims(state, axis=0))
+            action_values = np.reshape(np.squeeze(action_values), (64, 64))
+            action_space = self.env.project_legal_moves()  # The environment determines which moves are legal
+            action_values = np.multiply(action_values, action_space)
+            move_from = np.argmax(action_values, axis=None) // 64
+            move_to = np.argmax(action_values, axis=None) % 64
+            moves = [x for x in self.env.board.generate_legal_moves() if \
+                     x.from_square == move_from and x.to_square == move_to]
+            if len(moves) == 0:  # If all legal moves have negative action value, explore.
                 move = self.env.get_random_action()
                 move_from = move.from_square
                 move_to = move.to_square
             else:
-                action_values = self.agent.get_action_values(np.expand_dims(state, axis=0))
-                action_values = np.reshape(np.squeeze(action_values), (64, 64))
-                action_space = self.env.project_legal_moves()  # The environment determines which moves are legal
-                action_values = np.multiply(action_values, action_space)
-                move_from = np.argmax(action_values, axis=None) // 64
-                move_to = np.argmax(action_values, axis=None) % 64
-                moves = [x for x in self.env.board.generate_legal_moves() if \
-                         x.from_square == move_from and x.to_square == move_to]
-                if len(moves) == 0:  # If all legal moves have negative action value, explore.
-                    move = self.env.get_random_action()
-                    move_from = move.from_square
-                    move_to = move.to_square
-                else:
-                    move = np.random.choice(moves)  # If there are multiple max-moves, pick a random one.
+                move = np.random.choice(moves)  # If there are multiple max-moves, pick a random one.
 
-            episode_end, reward = self.env.step(move)
-            new_state = self.env.layer_board
-            if len(self.memory) > self.memsize:
-                self.memory.pop(0)
-                self.sampling_probs.pop(0)
-            turncount += 1
-            if turncount > maxiter:
-                episode_end = True
-                reward = 0
-            if episode_end:
-                new_state = new_state * 0
-            self.memory.append([state, (move_from, move_to), reward, new_state])
-            self.sampling_probs.append(1)
+        # Rest of the method remains the same
 
-            self.reward_trace.append(reward)
-
-            self.update_agent(turncount)
-
-        return self.env.board
 
     def sample_memory(self, turncount):
         """
